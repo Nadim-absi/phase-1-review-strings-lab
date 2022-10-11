a54aaf8a9a9cd8888bbe941866be4d9347481609
Nadim Absi <absinad1@gmail.com>
2022-10-11 16:17:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+let currentUser = "undefined";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+
 // Write your code in this file!
